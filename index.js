0483975e48ef2459b1048f6d14dd273babf0c3ce
syedrahmanbukhari92 <bukhari78615@gmail.com>
2026-01-28 11:37:14 +0500

    add vercil config

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const express = require('express');
 const {AccessToken} = require('livekit-server-sdk');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
+// Serve index.html on root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.post('/get-token', async (req, res) => {
   const { roomName, userName } = req.body;
@@ -30,6 +39,7 @@ app.post('/get-token', async (req, res) => {
   res.json({ token: await token.toJwt() });
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
